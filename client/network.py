d5a28aa658d384dabe86d9240230f36daaaed36a
Gavin Stark <gstark31897@gmail.com>
2016-05-17 20:51:46 -0500

    fixing style

--- client/network.py
+++ client/network.py
@@ -14,21 +14,21 @@ class ClientSocket:
       self.response_handlers = {}
       self.authenticated = False
 
-      self.connect_direct(address, port)
-      #self.authenticate_user()
+      self.connectDirect(address, port)
+      #self.authenticateUser()
 
-      self.processing_thread = threading.Thread(target=self.process_messages)
+      self.processing_thread = threading.Thread(target=self.processMessages)
       self.processing_thread.deamon = True
       self.processing_thread.start()
 
-   def connect_direct(self, address, port):
+   def connectDirect(self, address, port):
       self.sock = socket(AF_INET, SOCK_STREAM)
       self.sock.connect((address, port))
 
    def disconnect(self):
       self.sock.close()
 
-   def check_authentication(self, response):
+   def checkAuthentication(self, response):
       print("Checking Authentication")
       if response["good"] == "True":
          print("User Authenticated")
@@ -48,47 +48,47 @@ class ClientSocket:
          self.authenticated = False
          self.disconnect()
 
-   def authenticate_user(self):
+   def authenticateUser(self):
       request = {"action":"AUTH", "username":self.username, "password":self.password,
                  "reqnum":self.request_number}
-      self.response_handlers[self.request_number] = self.check_authentication
+      self.response_handlers[self.request_number] = self.checkAuthentication
       self.sendRequest(json.dumps(request))
 
-   def increment_request_number(self):
+   def incrementRequestNumber(self):
       self.request_number = (self.request_number + 1) % 256
 
-   def send_response(self, response):
+   def sendResponse(self, response):
       self.sock.send(json.dumps(response).encode("utf-8"))
 
    def sendRequest(self, request):
       self.sock.send(request.encode("utf-8"))
-      self.increment_request_number()
+      self.incrementRequestNumber()
 
-   def send_message(self, message, recipiant):
+   def sendMessage(self, message, recipiant):
       while not self.authenticated:
          time.sleep(1)
       request = {"action": "SEND", "recipiant": recipiant, "content": message}
       self.sendRequest(json.dumps(request))
 
-   def receive_message(self, request):
+   def receiveMessage(self, request):
       print("%s:%s" % (request["sender"], request["content"]))
       response = {"action":"RESP", "good":"True", "reqnum":request["reqnum"]}
-      send_response(response)
+      sendResponse(response)
 
-   def handle_message(self, message):
+   def handleMessage(self, message):
       message = json.loads(message)
       if message["action"] == "RESP" and message["reqnum"] in self.response_handlers:
          self.response_handlers[message["reqnum"]](message)
          del self.response_handlers[message["reqnum"]]
       elif message["action"] == "RECV":
-         self.receive_message(message)
+         self.receiveMessage(message)
 
-   def process_messages(self):
+   def processMessages(self):
       while True:
          data = self.sock.recv(1024)
          if len(data) == 0:
             break
-         self.handle_message(data.decode("utf-8"))
+         self.handleMessage(data.decode("utf-8"))
 
    def createUser(self):
       request = {"action":"CREATE", "username":"octalus", "password":"something", "email":"something", "reqnum":self.request_number}
@@ -98,4 +98,4 @@ class ClientSocket:
 
 socket = ClientSocket("localhost", 6667, "gstark", "potato")
 socket.createUser()
-socket.send_message("Hello", "gstark")
+socket.sendMessage("Hello", "gstark")
