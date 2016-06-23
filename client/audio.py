0af680dc245964db64e7b826e2aba8e0cb1659ba
Gavin Stark <gstark31897@gmail.com>
2016-06-22 22:25:43 -0500

    made audio p2p, working on simplifying

--- client/audio.py
+++ client/audio.py
@@ -4,21 +4,37 @@ import sys
 import threading
 
 
-frames = []
+output_frames = []
+input_frames = []
+port = int(sys.argv[1])
+peer_port = int(sys.argv[2])
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('localhost', port))
 
-def udp_stream(CHUNK):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(('localhost', 5001))
+#TODO merge the stream methods into one big one with a select statement
+def udp_stream_input():
     while True:
-        if len(frames) > 0:
-            data = sock.sendto(frames.pop(0), ('localhost', 5000))
-    sock.close()
+        if len(input_frames) > 0:
+            data = sock.sendto(input_frames.pop(0), ('localhost', peer_port))
 
 
-def record(stream, CHUNK):
+def udp_stream_output():
     while True:
-        frames.append(stream.read(CHUNK))
+        data, addr = sock.recvfrom(CHUNK * CHANNELS * 2)
+        output_frames.append(data)
+
+
+def play(stream):
+    while True:
+        if len(output_frames) > 10:
+            for i in range(0, len(output_frames)):
+                stream.write(output_frames.pop(0), CHUNK)
+
+
+def record(stream):
+    while True:
+        input_frames.append(stream.read(CHUNK))
 
 
 FORMAT = pyaudio.paInt16
@@ -28,13 +44,20 @@ RATE = 44100
 
 p = pyaudio.PyAudio()
 
-stream = p.open(format=FORMAT,
+input_stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True)
+output_stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                output=True)
 
-threading.Thread(target=udp_stream, args=[CHUNK]).start()
-threading.Thread(target=record, args=[stream, CHUNK]).start()
+
+threading.Thread(target=udp_stream_input).start()
+threading.Thread(target=udp_stream_output).start()
+threading.Thread(target=record, args=[input_stream]).start()
+threading.Thread(target=play, args=[output_stream]).start()
 
 #s.close()
 #stream.close()
